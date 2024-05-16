7b7065e5735faf9d6d8071c8f9fd095d260e17e4
Your Name <qaysgh02@gmail.com>
2024-05-16 12:45:07 -0500

    Created Gender.java
    Introduced Gender enum to represent gender values, by creating enum constants FEMALE and MALE with numeric representations to match the database representations.
    Provides type safety and avoids magic numbers for gender values.

---
+++ StudentDatabase/src/Source_Files/Gender.java
@@ -0,0 +1,13 @@
+package Source_Files;
+
+public enum Gender {
+
+    FEMALE(1),
+    MALE(2);
+
+    private final int description;
+
+    private Gender(int d){
+        description = d;
+    }
+}
