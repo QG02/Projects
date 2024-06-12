597719a635e3e1f32718fa2591fbcddc466e4dea
Your Name <qaysgh02@gmail.com>
2024-06-12 12:41:19 -0500

    removed project

--- StudentDatabase/src/Source_Files/Main.java
+++
@@ -1,9 +0,0 @@
-package Source_Files;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("New Project!!!");
-        //Is this where it began?
-    }
-}
