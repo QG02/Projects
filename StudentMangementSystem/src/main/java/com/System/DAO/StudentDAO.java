479449668efa2b487870a7af321526500941663a
Your Name <qaysgh02@gmail.com>
2024-06-13 19:38:04 -0500

    Created StudentDAO.java
    Created a DAO class for the student entity with a saveStudent method that persists a new student object into the database.

---
+++ StudentMangementSystem/src/main/java/com/System/DAO/StudentDAO.java
@@ -0,0 +1,38 @@
+package com.System.DAO;
+
+import com.System.ModelClass.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class StudentDAO {
+
+    // Instance of SessionFactory to manage Hibernate sessions
+    private final SessionFactory sessionFactory;
+
+    // Constructor to initialize the SessionFactory
+    public StudentDAO(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+     /**
+     * Method to save a Student object into the database.
+     * @param student: The student object to be saved.
+     */
+    public void saveStudent(Student student){
+        // Open a new session from the SessionFactory
+        Session session = sessionFactory.openSession();
+
+        // Begin a new transaction
+        Transaction transaction = session.beginTransaction();
+
+        // Save the student object using the persist method
+        session.persist(student);
+
+        // Commit the transaction to save changes to the database
+        transaction.commit();
+
+        // Close the session to release database connection
+        session.close();
+    }
+}
