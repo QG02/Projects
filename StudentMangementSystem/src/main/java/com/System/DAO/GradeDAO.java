46aa8e9c9f0e3155cbad4566ebb8ddb4ee072d91
Your Name <qaysgh02@gmail.com>
2024-06-19 19:24:34 -0500

    Created GradeDAO.java
    Created a DAO class for the grade entity with a saveGrade() method that persists a new grade object into the database, as well as a getGradeById() method that returns the info of a grade by the ID number.

---
+++ StudentMangementSystem/src/main/java/com/System/DAO/GradeDAO.java
@@ -0,0 +1,55 @@
+package com.System.DAO;
+
+import com.System.ModelClass.Grade;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class GradeDAO {
+
+    // Instance of SessionFactory to manage Hibernate sessions
+    private final SessionFactory sessionFactory;
+
+    // Constructor to initialize the SessionFactory
+    public GradeDAO(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    /**
+     * Method to save a grade object into the database.
+     * @param grade: The grade object to be saved.
+     */
+    public void saveGrade(Grade grade){
+
+        // Open a new session from the SessionFactory
+        Session session = sessionFactory.openSession();
+
+        // Begin a new transaction
+        Transaction transaction = session.beginTransaction();
+
+        // Save the grade object using the persist method
+        session.save(grade);
+
+        // Commit the transaction to save changes to the database
+        transaction.commit();
+
+        // Close the session to release database connection
+        session.close();
+    }
+
+    // Method to get a grade by ID
+    public Grade getGradeById(int gradeId) {
+
+        //Open a new session from the SessionFactory
+        Session session = sessionFactory.openSession();
+
+        //Load the fetched parameters into an empty grade object
+        Grade grade = session.get(Grade.class, gradeId);
+
+        //Close the session to release database connection
+        session.close();
+
+        //Return the newly constructed grade object
+        return grade;
+    }
+}
