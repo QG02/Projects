1803259b32ef3426e37e423fb2f993ae49ac6092
Your Name <qaysgh02@gmail.com>
2024-08-13 12:44:15 -0500

    Created MembershipType.java
    New enum type class to map the type of membership from the database.

---
+++ Gym_Membership/src/main/java/com/GymManagement/DTO/MembershipType.java
@@ -0,0 +1,29 @@
+package com.GymManagement.DTO;
+
+public enum MembershipType {
+
+    REGULAR("regular"),
+    DUO("duo"),
+    CHILD("child"),
+    SENIOR("senior"),
+    STUDENT("student");
+
+    private final String value;
+
+    MembershipType(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public MembershipType fromValue(String value){
+        for(MembershipType type: MembershipType.values()){
+            if(type.value.equalsIgnoreCase(value)){
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("UNKNOWN VALUE" + value);
+    }
+}
